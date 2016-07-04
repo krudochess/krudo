2d53b50f46f106648f8a3f3094007c9cad7857b2
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-04 11:28:02 +0200

    updated

--- tests/org/krudo/tests/perft/Perft1.java
+++ tests/org/krudo/tests/perft/Perft1.java
@@ -1,43 +1,61 @@
-package org.krudo.tests.benchmark;
+/**
+ * Krudo 0.16a - a chess engine for cooks
+ * by Francesco Bianco <bianco@javanile.org>
+ */
 
 //
-import static org.krudo.util.Debug.*;
+package org.krudo.tests.perft;
+
+//
+import org.krudo.*;
+
+//
 import static org.krudo.util.Tool.*;
+import static org.krudo.util.Debug.*;
 
 //
-import org.krudo.Node;
-
-//
-public class Perft1 {
-
+public class Perft1 
+{
     //
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {   
         /*\
-        FEN: STARTPOS
-        20            0 ms
-        400            16 ms
-        8902        141 ms
-        197281        173 ms
-        4865609        2313 ms
-        119060324    51176 ms
+        FEN:       STARTPOS
+        20         0 ms
+        400        16 ms
+        8902       141 ms
+        197281     173 ms
+        4865609    2313 ms
+        119060324  51176 ms
         \*/
     
+        //
+        Moves.init();
+        
         //
         Node n = new Node();
         
         //
-        try {
+        n.startpos();
+        
+        //
+        try
+        {
             /*_*/
-            for(int i=1; i<=6; i++) {
-                echo(perft(n,i));
+            for (int i=1; i<=6; i++) 
+            {
+                echo(perft(n, i)) ;
             }
             //echo (Cache.legals.size());
             //dump(n);
             /*/
             Perft.table(n,5);
             /*_*/
-        } catch (Exception e) {
+        } 
+        
+        //
+        catch (Exception e) 
+        {
             dump(n);
             dump(n.L);
             dump(e);
