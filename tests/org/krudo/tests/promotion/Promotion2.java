322ebb6d7ef3788f81f8b65d8ec0df90ecea7f7c
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-05 12:00:39 +0200

    updated

---
+++ tests/org/krudo/tests/promotion/Promotion2.java
@@ -0,0 +1,36 @@
+/**
+ * Krudo 0.16a - a chess engine for cooks
+ * by Francesco Bianco <bianco@javanile.org>
+ */
+
+//
+package org.krudo.tests.promotion;
+
+//
+import org.krudo.*;
+
+//
+import static org.krudo.util.Debug.*;
+
+// 
+public class Promotion2 
+{
+    //
+    public static void main(String[] args) 
+    {              
+        //
+        Moves.init();
+
+        // create a node to service the search
+        Node n = new Node();
+        
+        //
+        n.startpos("k7/8/8/8/8/8/p6P/7K b");
+        
+        //
+        dump(n);
+        
+        //
+        dump(n.legals());
+    }    
+}
