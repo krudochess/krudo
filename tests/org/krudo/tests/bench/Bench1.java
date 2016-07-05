dd907fd9a945b9eb61ce3f47f473b12809189b8f
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-05 12:40:23 +0200

    updated

--- tests/org/krudo/tests/bench/Bench1.java
+++ tests/org/krudo/tests/bench/Bench1.java
@@ -13,11 +13,11 @@ import org.krudo.Line;
 import static org.krudo.util.Tool.*;
 
 //
-public class Bench1 {
-
+public class Bench1 
+{
     //
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         //
         Line l = new Line();
 
@@ -28,45 +28,77 @@ public class Bench1 {
         long t = time();
         
         //
-        for (int n=0; n<300; n++) {
-        
+        for (int r = 0; r<10; r++) 
+        {
             //
-            l.store(
-                n, 
-                rand(),
-                rand(),
-                rand(),
-                rand(),
-                rand(),
-                rand(),
-                rand() 
-            );
-            
-            //
-            for (int i=0; i<300; i++) {
+            for (int n=0; n<300; n++)
+            {
+                //
+                l.store(
+                    n, 
+                    rand(),
+                    rand(),
+                    rand(),
+                    rand(),
+                    rand(),
+                    rand(),
+                    rand() 
+                );
 
                 //
-                for (int j=0; j<300; j++) {
+                for (int i=0; i<300; i++)
+                {
+                    //
+                    for (int j=0; j<300; j++) 
+                    {
+                        //
+                        l.store(
+                            j, 
+                            l.p[i],
+                            l.s[i],
+                            l.v[i],
+                            l.x[i],
+                            l.k[i],
+                            l.e[i],
+                            l.c[i] 
+                        );
 
+                        //
+                        c++;
+                    }
+                }
+
+                //
+                for (int i=0; i<300; i++)
+                {
                     //
-                    l.store(
-                        j, 
-                        l.p[i],
-                        l.s[i],
-                        l.v[i],
-                        l.x[i],
-                        l.k[i],
-                        l.e[i],
-                        l.c[i] 
-                    );
-                    
-                    //
-                    c++;
+                    for (int j=0; j<300; j++) 
+                    {
+                        //
+                        l.store(
+                            j, 
+                            l.p[i],
+                            l.s[i],
+                            l.v[i],
+                            l.x[i],
+                            l.k[i],
+                            l.e[i],
+                            l.c[i] 
+                        );
+
+                        //
+                        c++;
+                    }
                 }
             }
         }
         
         //
-        echo(time() - t, "ms", c, "loops");
+        print(time() - t, "ms", c, "loops");
+        
+        /*
+        2424 ms 540000000 loops
+        2451 ms 540000000 loops
+        */
     }    
 }
