61b9ec668dbc4145840bdeeaee9bfd4ec1618f55
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-05 23:43:55 +0200

    piccola gestione carriera

--- tests/org/krudo/tests/capture/Capture1.java
+++ tests/org/krudo/tests/capture/Capture1.java
@@ -9,39 +9,42 @@ import static org.krudo.util.Tool.*;
 import org.krudo.Node;
 
 // 
-public class Capture1 {
-
+public class Capture1 
+{
     //
     public static void main(String[] args)
     {
-         //
+        //
         Moves.init();
 
         // 
         Node n = new Node();
 
-         //
+        //
         n.startpos();
 
+        //
         try
         {
-           
-
-           
             // do move e2e4
             n.domove("e2e4 b8a6".split("\\s"));
 
+            //
             n.domove(n.legals(), 14);
             
             // print out position
             dump(n);   
 
+            //
             dump(n.legals());  
         } 
         
         catch (Exception e) 
         {
+            //
             dump(n);
+            
+            //
             e.printStackTrace();
         }
     }    
