9692fbd4ed510825ebb7d8257342a918ca847a0a
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-06 11:21:19 +0200

    updated

--- tests/org/krudo/tests/perft/Perft2.java
+++ tests/org/krudo/tests/perft/Perft2.java
@@ -4,44 +4,55 @@
  */
 
 //
-package org.krudo.tests.benchmark;
+package org.krudo.tests.perft;
+
+//
+import org.krudo.*;
 
 //
 import static org.krudo.util.Debug.*;
 import static org.krudo.util.Tool.*;
 
 //
-import org.krudo.Node;
-
-//
-public class Perft2 {
-
+public class Perft2 
+{
     //
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {    
         /*\
         FEN: r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -
-        48            0 ms
-        2039        0 ms
-        97862        0 ms
-        4085603        0 ms
-        193690690    0 ms        
+        48         0 ms
+        2039       0 ms
+        97862      0 ms
+        4085603    0 ms
+        193690690  0 ms        
         \*/
-                
-        //Node n = new Node("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+        
+        Moves.init();
+        
         Node n = new Node();
         
-        n.startpos("3k4/8/8/8/8/8/8/4K3 w KQkq -");
-                
-        try {
+        n.startpos("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+             
+        //dump(n);
+        
+        //dump(n.legals());
+        
+        try 
+        {
             /*_*/
-            for(int i=1; i<=10; i++) {
-                echo(perft(n,i));
+            for (int i = 1; i <= 2; i++)
+            {
+                print(perft(n,i));
             }
             /*/
             Perft.table(n,5);
             /*_*/
-        } catch (Exception e) {
+        } 
+        
+        //
+        catch (Exception e)
+        {
             dump(n);
             dump(n.L);
             dump(e);
