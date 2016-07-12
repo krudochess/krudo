74b53baa71c0803146c0b72af979b97e7a593a7c
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-13 01:14:36 +0200

    piccola gestione carriera

---
+++ tests/org/krudo/tests/eval/Eval1.java
@@ -0,0 +1,51 @@
+/**
+ * Krudo 0.16a - a chess engine for cooks
+ * by Francesco Bianco <bianco@javanile.org>
+ */
+
+//
+package org.krudo.tests.eval;
+
+//
+import static org.krudo.Constant.*;
+import static org.krudo.util.Debug.*;
+import static org.krudo.util.Tool.*;
+import static org.krudo.util.Decode.*;
+
+//
+import org.krudo.*;
+
+//
+public class Eval1 
+{
+    public final static int[] a = new int[] {1,2,3}; 
+    
+    public final static int[] b = new int[] {5,5,5}; 
+    
+    static {
+        b[0] = b[0] - a[0];
+    }
+    
+	//
+	public static void main(String[] args) 
+    {
+
+        int o = -100;
+        int f = 200 - o;
+        
+        int p = 250;
+        
+        //print(o + (p * f >> 8));
+        print(b[0]);
+        int m = 0b11;
+        
+        print(bin(wq));
+        print(bin(bq));
+        print(bin(wr));
+        print(bin(wb));
+        print(bin(wn));
+        print(wn & m);
+        
+	}
+}
+
