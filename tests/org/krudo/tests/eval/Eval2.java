13b2cb04c2bc71b074631e404242d35d77b5f201
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-13 13:56:35 +0200

    intriduzione concetto di convalidato

---
+++ tests/org/krudo/tests/eval/Eval2.java
@@ -0,0 +1,44 @@
+/**
+ * Krudo 0.16a - a chess engine for cooks
+ * by Francesco Bianco <bianco@javanile.org>
+ */
+
+//
+package org.krudo.tests.eval;
+
+//
+import static org.krudo.Constant.*;
+import static org.krudo.util.Debug.*;
+import static org.krudo.util.Tool.*;
+import static org.krudo.util.Decode.*;
+
+//
+import org.krudo.*;
+
+//
+public class Eval2 
+{
+    
+    
+	//
+	public static void main(String[] args) 
+    {
+
+       Moves.init();
+           
+           
+       Node n = new Node();    
+        
+       n.startpos();
+       
+       n.legals();
+       
+       n.domove("e2e4");
+       
+       print(Eval.node(n));
+       
+       Eval.dump();
+     
+	}
+}
+
