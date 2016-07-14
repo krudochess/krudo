ae6fc0c6245304b1b1c9694283bf812df6710666
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-14 17:37:37 +0200

    intriduzione concetto di convalidato

---
+++ tests/org/krudo/tests/search/Search5.java
@@ -0,0 +1,37 @@
+/**
+ * Krudo 0.16a - a chess engine for cooks
+ * by Francesco Bianco <bianco@javanile.org>
+ */
+
+//
+package org.krudo.tests.search;
+
+import org.krudo.Moves;
+import static org.krudo.util.Tool.*;
+
+//
+import org.krudo.Node;
+import org.krudo.Search;
+
+// 
+public class Search5 
+{
+    //
+    public static void main(String[] args) 
+    {
+        //
+        Moves.init();
+        
+        // create a node to service the search
+        Node n = new Node();
+        
+        //
+        n.startpos();
+        
+        // create a serach engine based-on the node
+        Search s = new Search(n);
+     
+        //
+        s.start(6);       
+    }    
+}
