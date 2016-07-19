acd57566dc37b54c4ba329057cffe32fdca230f3
Francesco Bianco <info.francescobianco@gmail.com>
2016-07-19 14:01:37 +0200

    intriduzione concetto di convalidato

--- work/perft.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java -cp ..\build\classes org.krudo.test.bench.Perft1
--- work/vs-krudo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-xboard -fcp /home/cicciodarkast/Progetti/Piriton/rel/piriton.sh -fd /home/cicciodarkast/Progetti/Piriton/rel/ -scp  /usr/games/fairymax -sd /home/cicciodarkast/Progetti/Piriton/rel/ -mg 1
