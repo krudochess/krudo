8504d5615dda794632b87d6638daf165c8e495bb
Francesco Bianco <info.francescobianco@gmail.com>
2016-11-06 18:23:14 +0100

    introducing bin folder for executables

--- console/config.sh
+++ console/config.sh
@@ -6,21 +6,19 @@
 #
 
 # commands
-JAVA=java
-PYTHON=python
-XBOARD=xboard
-POLYGLOT=polyglot
-POSTGAME="$PYTHON $KRUDO_DIR/../utils/postgame.py"
-GIN=gin
+export JAVA=java
+export PYTHON=python
+export XBOARD=xboard
+export POLYGLOT=polyglot
+export POSTGAME="$PYTHON $KRUDO_DIR/../utils/postgame.py"
+export GIN=gin
 
 # engine vars
-KRUDO_DIR="$( cd "$( dirname "$0" )" && pwd )"
-KRUDO_GIN="krudo.gin"
-KRUDO_INI=krudo.ini
-KRUDO_VER=0.16a
-KRUDO_IMG=$KRUDO_DIR/logo.bmp
-KRUDO_LOG=krudo.log
-KRUDO_TAG="Krudo $KRUDO_VER"
-KRUDO_CMD="$JAVA -cp $KRUDO_DIR/../build/classes org.krudo.Krudo" 
-
-AAAA=100
+export KRUDO_DIR="$( cd "$( dirname "$0" )" && pwd )"
+export KRUDO_GIN="krudo.gin"
+export KRUDO_INI=krudo.ini
+export KRUDO_VER=0.16a
+export KRUDO_IMG=$KRUDO_DIR/logo.bmp
+export KRUDO_LOG=krudo.log
+export KRUDO_TAG="Krudo $KRUDO_VER"
+export KRUDO_CMD="$JAVA -cp $KRUDO_DIR/../build/classes org.krudo.Krudo" 
