2a35bd39196f3248c77e7280b180c84b215c7e81
Francesco Bianco <info.francescobianco@gmail.com>
2016-11-06 16:22:02 +0100

    introducing bin folder for executables

---
+++ console/config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Krudo 0.16a - a chess engine for cooks
+# by Francesco Bianco <bianco@javanile.org>
+#
+
+# commands
+JAVA=java
+PYTHON=python
+XBOARD=xboard
+POLYGLOT=polyglot
+POSTGAME="$PYTHON $KRUDO_DIR/../utils/postgame.py"
+GIN=gin
+
+# engine vars
+KRUDO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KRUDO_GIN="krudo.gin"
+KRUDO_INI=krudo.ini
+KRUDO_VER=0.16a
+KRUDO_IMG=$KRUDO_DIR/logo.bmp
+KRUDO_LOG=krudo.log
+KRUDO_TAG="Krudo $KRUDO_VER"
+KRUDO_CMD="$JAVA -cp $KRUDO_DIR/../build/classes org.krudo.Krudo" 
+
