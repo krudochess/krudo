52656d25b96f6079b99c6dbdff64ab7e1b9e72db
francescobianco <info.francescobianco@gmail.com>
2017-07-14 23:12:36 +0200

    new debug tecnique

--- tests/org/krudo/tests/attack/Attack1.java
+++ tests/org/krudo/tests/attack/Attack1.java
@@ -3,36 +3,27 @@
  * by Francesco Bianco <bianco@javanile.org>
  */
 
-//
 package org.krudo.tests.attack;
 
-//
 import org.krudo.*;
 
-//
 import static org.krudo.Tool.*;
 import static org.krudo.tests.debug.Debug.*;
 import static org.krudo.Constants.*;
 
-//
 public class Attack1 
 {
     //
     public static void main(String[] args) 
     {
-        //
         DEBUG_SHOW_MOVE_WEIGHT = true;
         
-        //
         Node n = new Node();
 
-        //
         n.startpos();
 
-        //
         n.domove("d2d3");
-        
-        //
+
         print(n.white_attack(e4));
     }    
 }
