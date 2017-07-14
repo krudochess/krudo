381ee05959773fa0409b16e25fef923eca3fee70
francescobianco <info.francescobianco@gmail.com>
2017-07-15 00:07:52 +0200

    new debug tecnique

--- tests/org/krudo/tests/winatchess/Winatchess1.java
+++ tests/org/krudo/tests/winatchess/Winatchess1.java
@@ -3,132 +3,73 @@
  * by Francesco Bianco <bianco@javanile.org>
  */
 
-// 
 package org.krudo.tests.winatchess;
 
-//
 import org.krudo.*;
 
-//
 import static org.krudo.Tool.*;
-import static org.krudo.tests.debug.Debug.*;
 import static org.krudo.Describe.*;
+import static org.krudo.tests.debug.Debug.*;
 
-// 
 public class Winatchess1 
 {
-    //
     public static void main(String[] args) 
     {        
-        //
-        String[][] epd = EPDReader("./tests/org/krudo/tests/winatchess/WinAtChess.epd");
+        DEBUG_SHOW_MOVE_WEIGHT = true;
 
-        //
+        String[][] epd = EPDReader("./positions/Win-at-Chess.epd");
+
         Krudo.init();
-        
-        //
-        Search s = new Search();
-          
-        //
+
         Timer t = new Timer();
         
-        //
+        Search s = new Search();        
         s.send_text_info = (search) -> {};
-        
-        //
         s.send_best_move = (search) -> {};
-        
-        //
-        int d = 5;
-        
-        //
-        long a = 3500;
-        
-        //
-        int p = 0;
-        
-        //
-        int o = 0;
-        
-        //
-        int r = 0;
-        
-        //
-        int h = 400;
-        
-        //
-        int l = epd.length;
-        
-        //l = 1;
-        
-        //
-        for (int i = 0; i < (l-o); i++)
-        {            
-            //
+                        
+        int depth = 5;
+        int time = 3500;             
+        int epd_offset = 0;
+        int count_success = 0;                            
+        int success_ratio = 0;                
+        int fail_limit = 10;
+
+        for (int i = epd_offset; i < epd.length; i++) {            
             t.start();
                         
-            //
-            String[] row = epd[i + o];
-            
-            //                        
+            String[] row = epd[i];
             //print(row[1]);
-                                   
-            //
             s.node.startpos(row[1]);
-            
-            //
             //dump(n);
-            
-            //
-            try 
-            {
-                s.start(d, a);
-            } 
-            
-            //
-            catch (Exception e)
-            {                
+                        
+            try {
+                s.start(depth, time);
+            } catch (Exception e) {                
                 dump(s.node);
                 dump(s.node.L);                
                 e.printStackTrace();
             }
             
-            //
             String bm = algebric(s.node, s.id_best_move);
             
-            //
-            if (row[2].equals(bm)) 
-            { 
-                //
-                p++;
-                
-                //
-                r = 100 * p / (i+1);
-                
-                //
+            if (row[2].equals(bm)) { 
+                count_success++;
+                success_ratio = 100 * count_success / (i+1);
                 t.stamp();
-                
-                //
-                print(row[0]+" ("+r+"%):", row[2], "==", bm, "  ("+(t.stamp/1000)+" sec.)");                                    
-            }
-            
-            //
-            else
-            {
-                //
-                r = 100 * p / (i+1);
-                
-                //
-                print(row[0]+" ("+r+"%):", row[2], "!=", bm); 
-                
-                //
-                h--;
-                
-                //
-                if (h < 0) 
-                {
-                    //
-                    DEBUG_SHOW_MOVE_WEIGHT = true;
+                print(
+                    row[0], 
+                    "("+success_ratio+"%):", 
+                    row[2], 
+                    "==", 
+                    bm, 
+                    "("+(t.stamp/1000)+" sec.)"
+                );                                    
+            } else {
+                success_ratio = 100 * count_success / (i+1);
+                print(row[0], "("+success_ratio+"%):", row[2], "!=", bm); 
+                fail_limit--;
+
+                if (fail_limit < 0) {
                     dump(s.node);
                     s.node.legals();
                     dump(s.node.legals);
@@ -136,8 +77,7 @@ public class Winatchess1
                 }
             }
         }        
-        
-        //
-        print("Result:", p+"/"+l);
+                
+        print("Result:", count_success+"/"+epd.length);
     }    
 }
